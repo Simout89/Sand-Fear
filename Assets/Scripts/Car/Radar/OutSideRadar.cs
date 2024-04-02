aa85b005ba2fd91bb6425c4ad91f8be25c09d91a
Sim_out <alik2006sootkill@gmail.com>
2024-04-02 21:03:13 +0700

    update

--- Assets/Scripts/Car/Radar/OutSideRadar.cs
+++ Assets/Scripts/Car/Radar/OutSideRadar.cs
@@ -15,13 +15,17 @@ public class OutSideRadar : MonoBehaviour
     [SerializeField] private float Radius = 1f;
     [SerializeField] private float Speed = 1f;
     [SerializeField] private float PointSpawnSpeed = 1f;
+    [SerializeField] private float PingDelaySec = 1f;
 
     [SerializeField] private GameObject Pivot;
     [SerializeField] private GameObject GreenPoint;
     [SerializeField] private GameObject YellowPoint;
     [SerializeField] private GameObject Target;
+    [SerializeField] private AudioClip RadarPing;
+    [SerializeField] private AudioSource audioSource;
 
     private bool delay = true;
+    private bool pingDelay = true;
     private void Awake()
     {
         transform.DOLocalRotate(new Vector3(0, 360, 0), Speed, RotateMode.FastBeyond360).SetLoops(-1, LoopType.Restart).SetEase(Ease.Linear).SetRelative();
@@ -41,6 +45,11 @@ public class OutSideRadar : MonoBehaviour
                     StartCoroutine(Delay());
                 }else
                 {
+                    if(pingDelay)
+                    {
+                        audioSource.PlayOneShot(RadarPing);
+                        StartCoroutine(PingDelay());
+                    }
                     Instantiate(GreenPoint, Target.transform.position, Quaternion.Euler(0, 0, Pivot.transform.rotation.eulerAngles.z));
                     StartCoroutine(Delay());
                 }
@@ -58,5 +67,11 @@ public class OutSideRadar : MonoBehaviour
         delay = false;
         yield return new WaitForSeconds(PointSpawnSpeed);
         delay = true;
+    }
+    private IEnumerator PingDelay()
+    {
+        pingDelay = false;
+        yield return new WaitForSeconds(PingDelaySec);
+        pingDelay = true;
     }
 }
