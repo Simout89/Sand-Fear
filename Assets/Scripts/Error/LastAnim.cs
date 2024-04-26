9545fbd38fe1b024559145a26a032a6459268d0a
Sim_out <alik2006sootkill@gmail.com>
2024-04-26 22:50:12 +0700

    update

--- Assets/Scripts/Error/LastAnim.cs
+++ Assets/Scripts/Error/LastAnim.cs
@@ -7,6 +7,15 @@ public class LastAnim : MonoBehaviour
 {
     [SerializeField] private Behaviour[] playerControllers;
     [SerializeField] private float delay = 1f;
+    [SerializeField] private Transform parent; 
+    [SerializeField] private Animator animator; 
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         for (int i = 0; i < playerControllers.Length; i++)
@@ -14,6 +23,10 @@ public class LastAnim : MonoBehaviour
             playerControllers[i].enabled = false;
             
         }
+        other.gameObject.transform.parent = parent;
+        other.gameObject.transform.localPosition = Vector3.zero;
+        animator.SetTrigger("Eat");
+        audioSource.PlayOneShot(audioSource.clip);
         StartCoroutine(Delay());
     }
 
